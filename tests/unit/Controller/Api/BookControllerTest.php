355b149aaccba8925f1d4a994a73f1c197c28563
jules <judigonnet@gmail.com>
2024-03-28 20:49:52 +0100

    Coding Standard

---
+++ tests/unit/Controller/Api/BookControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\unit\Controller\Api;
+
+use App\Controller\Api\BookController;
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
+
+class BookControllerTest extends TestCase
+{
+
+}
